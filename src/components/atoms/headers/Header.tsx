8c0ac0ebe922351e9018351cd14d0c2543a1f360
Mark <markwilkins86@gmail.com>
2024-01-08 10:57:03 -0500

    Rewrote Header Component with Styled Component pieces

--- src/components/atoms/headers/Header.tsx
+++ src/components/atoms/headers/Header.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import styled from '@mui/system/styled';
 
-const Header: React.FC = () => {
+const HeaderContainer = styled('div')({
+  margin: '20px',
+  fontWeight: 'bold',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  color: 'white'
+});
+
+const TitleContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+});
+
+const FirstName = styled('p')(({ theme }) => ({
+  margin: 0,
+  fontSize: '50px',
+  [theme.breakpoints.up('lg')]: {
+    fontSize: '65px', 
+  },
+}));
+
+const LastName = styled('p')(({ theme }) => ({
+  margin: 0,
+  fontSize: '15px',
+  marginTop: '-10px',
+  [theme.breakpoints.up('lg')]: {
+    fontSize: '20px', 
+  },
+}));
+
+const Header = () => {
   return (
-    <div className="header">
-      <div className="title">
-        <p className="firstname">KURT</p>
-        <p className="lastname">SENSENBRENNER</p>
-      </div>
-    </div>
-  )
-}
+    <HeaderContainer>
+      <TitleContainer>
+        <FirstName>KURT</FirstName>
+        <LastName>SENSENBRENNER</LastName>
+      </TitleContainer>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
