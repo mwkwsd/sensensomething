f52cdfed7ef857dd31dcfd2e8ccac7c0075a37a8
Mark <markwilkins86@gmail.com>
2024-01-10 16:34:53 -0500

    header

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Contact from "./components/templates/Contact";
 import { VideoListPage } from "./components/templates/VideoListPage";
 import SeriesPage from "./components/templates/SeriesPage";
 import AnimationPage from "./components/templates/AnimationPage";
+import { Header } from "./components/molecules/headers/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
