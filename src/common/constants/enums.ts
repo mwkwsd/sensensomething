f9f8fbd899f9dc0bd962653a7e6dc11970a6235d
WillyLS <WLScripps@gmail.com>
2024-01-09 22:17:08 -0700

    Fix type checker function

--- src/common/constants/enums.ts
+++ src/common/constants/enums.ts
@@ -1,10 +1,10 @@
 const roles = ["director_producer", "camera_operator"] as const;
 export type Role = (typeof roles)[number];
 export function roleTypeChecker(arg: any): arg is Role {
-  return arg in roles;
+  return roles.includes(arg);
 }
 const genres = ["documentary", "animation", "series"] as const;
 export type Genre = (typeof genres)[number];
 export function genreTypeChecker(arg: any): arg is Genre {
-  return arg in genres;
+  return genres.includes(arg);
 }
