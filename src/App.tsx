f56aca16e382dc3c983409e24b132465ce450575
Mark <markwilkins86@gmail.com>
2024-01-10 16:34:11 -0500

    removed Header import - causing merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import Contact from "./components/templates/Contact";
 import { VideoListPage } from "./components/templates/VideoListPage";
 import SeriesPage from "./components/templates/SeriesPage";
 import AnimationPage from "./components/templates/AnimationPage";
-import { Header } from "./components/molecules/headers/Header";
 
 function App() {
   return (
     <div>
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
