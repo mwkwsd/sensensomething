b833d1df35104a136bd2ec0a3ba19b43bd4c0575
Mark <markwilkins86@gmail.com>
2024-01-10 16:32:38 -0500

    importing Header to App for visibilty

--- src/components/molecules/headers/Header.tsx
+++ src/components/molecules/headers/Header.tsx
@@ -1,42 +1,31 @@
-import React from 'react';
-import styled from '@mui/system/styled';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { FirstName } from '../../atoms/firstName/FirstName';
-import { LastName } from '../../atoms/lastName/LastName';
+import React from "react";
+import styled from "@mui/system/styled";
+import { FirstName } from "../../atoms/firstName/FirstName";
+import { LastName } from "../../atoms/lastName/LastName";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#2196f3',
-    },
-  },
+const HeaderContainer = styled("div")({
+  margin: "20px",
+  fontWeight: "bold",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  color: "white",
 });
 
-const HeaderContainer = styled('div')({
-  margin: '20px',
-  fontWeight: 'bold',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  color: 'white',
-});
-
-const TitleContainer = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
+const TitleContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
 });
 
 export function Header() {
   return (
-    <ThemeProvider theme={theme}>
-      <HeaderContainer>
-        <TitleContainer>
-          <FirstName firstName='KURT' />
-          <LastName lastName="SENSENBRENNER" />
-        </TitleContainer>
-      </HeaderContainer>
-    </ThemeProvider>
+    <HeaderContainer>
+      <TitleContainer>
+        <FirstName firstName="KURT" />
+        <LastName lastName="SENSENBRENNER" />
+      </TitleContainer>
+    </HeaderContainer>
   );
 }
 
