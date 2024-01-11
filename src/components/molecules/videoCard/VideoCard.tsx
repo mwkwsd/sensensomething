adc53fa65714440ca39aef029dcfc5bfc1df0d30
WillyLS <WLScripps@gmail.com>
2024-01-10 18:40:22 -0700

    Update video to have list of genres

--- src/components/molecules/videoCard/VideoCard.tsx
+++ src/components/molecules/videoCard/VideoCard.tsx
@@ -7,22 +7,33 @@ import { VideoInfo } from "../../atoms/videoInfo/VideoInfo";
 import { IVideoCard } from "../../../common/interfaces/IVideoCard";
 import { convertFromEnumToText } from "../../../common/utils/utils";
 
-type VideoCardProps = Omit<IVideoCard, "isRecentWork" | "genre">;
+type VideoCardProps = Omit<IVideoCard, "isRecentWork">;
 
 // TODO: Video needs to take full width of Card
-export function VideoCard({ url, title, clientName, roles }: VideoCardProps) {
+export function VideoCard({
+  url,
+  title,
+  clientName,
+  roles,
+  genres,
+}: VideoCardProps) {
   const mediaCardVideo = useMemo(() => <Video url={url} />, [url]);
   const videoTitle = useMemo(() => <VideoTitle title={title} />, [title]);
 
   const clientComponent = useMemo(
-    () => <VideoInfo label="Client" text={clientName} />,
+    () => <VideoInfo label="Client" infoValue={clientName} />,
     [clientName]
   );
 
   const rolesComponent = useMemo(() => {
-    const rolesText = roles.map(convertFromEnumToText).join(", ");
-    return <VideoInfo label="Roles" text={rolesText} />;
+    const rolesComponents = roles.map(convertFromEnumToText).join(", ");
+    return <VideoInfo label="Roles" infoValue={rolesComponents} />;
   }, [roles]);
+
+  const genresComponent = useMemo(() => {
+    const genresComponents = genres.map(convertFromEnumToText).join(", ");
+    return <VideoInfo label="Genres" infoValue={genresComponents} />;
+  }, [genres]);
 
   return (
     <Card>
@@ -31,6 +42,7 @@ export function VideoCard({ url, title, clientName, roles }: VideoCardProps) {
         {videoTitle}
         {clientComponent}
         {rolesComponent}
+        {genresComponent}
       </CardContent>
     </Card>
   );
