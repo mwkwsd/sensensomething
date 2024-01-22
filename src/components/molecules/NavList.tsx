0bfae6ceae67d2b5668ee77bffb82968633d0321
Mark <markwilkins86@gmail.com>
2024-01-22 13:50:18 -0500

    Wasn't good enough. Even more concise.

--- src/components/molecules/NavList.tsx
+++ src/components/molecules/NavList.tsx
@@ -1,11 +1,12 @@
-import { List } from '@mui/material'
-import { useLocation } from 'react-router-dom'
-import { RecentWorkButton } from '../atoms/buttons/recentWorkButton/RecentWorkButton'
-import { AboutButton } from '../atoms/buttons/aboutButton/AboutButton'
-import { ContactButton } from '../atoms/buttons/contactButton/ContactButton'
-import { SubheaderList } from './subheaderList/SubheaderList'
-import { Genre, Role } from '../../common/constants/enums'
-import { genreToText, roleToText } from '../../common/constants/constants'
+import React from 'react';
+import { List } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { RecentWorkButton } from '../atoms/buttons/recentWorkButton/RecentWorkButton';
+import { AboutButton } from '../atoms/buttons/aboutButton/AboutButton';
+import { ContactButton } from '../atoms/buttons/contactButton/ContactButton';
+import { SubheaderList } from './subheaderList/SubheaderList';
+import { Genre, Role } from '../../common/constants/enums';
+import { genreToText, roleToText } from '../../common/constants/constants';
 
 const commonStyle = {
   fontWeight: 'bold',
@@ -13,13 +14,19 @@ const commonStyle = {
   color: 'white',
   fontSize: 20,
   textAlign: 'right',
-}
+};
+
 interface ListButtonProps {
   label: string;
   filter: Role | Genre;
   isSelected: boolean;
   generateButtonStyle: (isSelected: boolean) => React.CSSProperties;
   isLinkSelected: (link: string) => boolean;
+}
+
+interface NavListProps {
+  title: string;
+  items: Record<string, string>;
 }
 
 export function NavList() {
@@ -31,13 +38,13 @@ export function NavList() {
   };
 
   const generateButtons = (items: Record<string, string>, filterPrefix: string): ListButtonProps[] =>
-  Object.entries(items).map(([key, label]) => ({
-    label,
-    filter: key as Role | Genre,
-    isSelected: isLinkSelected(`/video-list?filter=${filterPrefix}${key}`),
-    generateButtonStyle,
-    isLinkSelected,
-  }));
+    Object.entries(items).map(([key, label]) => ({
+      label,
+      filter: key as Role | Genre,
+      isSelected: isLinkSelected(`/video-list?filter=${filterPrefix}${key}`),
+      generateButtonStyle,
+      isLinkSelected,
+    }));
 
   const generateButtonStyle = (isSelected: boolean) => ({
     backgroundColor: isSelected ? 'lightgrey' : 'transparent',
@@ -49,8 +56,10 @@ export function NavList() {
     color: isSelected ? 'black' : 'white',
   });
 
-  const roleButtons: ListButtonProps[] = generateButtons(roleToText, '');
-  const genreButtons: ListButtonProps[] = generateButtons(genreToText, '');
+  const navListItems: NavListProps[] = [
+    { title: 'ROLE', items: roleToText },
+    { title: 'GENRE', items: genreToText },
+  ];
 
   return (
     <List sx={{ width: '100%', height: '100%', maxWidth: 360, ...commonStyle }} component="nav">
@@ -58,8 +67,15 @@ export function NavList() {
       <AboutButton isSelected={isLinkSelected('/about')} generateButtonStyle={generateButtonStyle} isLinkSelected={isLinkSelected} />
       <ContactButton isSelected={isLinkSelected('/contact')} generateButtonStyle={generateButtonStyle} isLinkSelected={isLinkSelected} />
 
-      <SubheaderList title="ROLE" buttons={roleButtons} generateButtonStyle={generateButtonStyle} isLinkSelected={isLinkSelected} />
-      <SubheaderList title="GENRE" buttons={genreButtons} generateButtonStyle={generateButtonStyle} isLinkSelected={isLinkSelected} />
+      {navListItems.map((item) => (
+        <SubheaderList
+          key={item.title}
+          title={item.title}
+          buttons={generateButtons(item.items, '')}
+          generateButtonStyle={generateButtonStyle}
+          isLinkSelected={isLinkSelected}
+        />
+      ))}
     </List>
   );
 }
