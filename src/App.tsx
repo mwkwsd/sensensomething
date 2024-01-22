d5694609333d9708c216bc600c8d856b6b8bfec8
Mark <markwilkins86@gmail.com>
2024-01-22 14:12:32 -0500

    div to fragment on App.tsx. update prettier
    git push origin header --force

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/templates/Home";
-import Contact from "./components/templates/Contact";
-import { VideoListPage } from "./components/templates/VideoListPage";
-import SeriesPage from "./components/templates/SeriesPage";
-import AnimationPage from "./components/templates/AnimationPage";
-import { Header } from "./components/molecules/headers/Header";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/templates/Home'
+import Contact from './components/templates/Contact'
+import { VideoListPage } from './components/templates/VideoListPage'
+import SeriesPage from './components/templates/SeriesPage'
+import AnimationPage from './components/templates/AnimationPage'
+import { Header } from './components/molecules/headers/Header'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <BrowserRouter>
         <Routes>
@@ -20,8 +20,8 @@ function App() {
           <Route path="/animation" element={<AnimationPage />} />
         </Routes>
       </BrowserRouter>
-    </div>
-  );
+    </>
+  )
 }
 
 export default App
