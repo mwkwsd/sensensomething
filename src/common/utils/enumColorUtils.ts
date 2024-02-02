58c914e338c3df933a30cca2be7113235d5afc71
Mark <markwilkins86@gmail.com>
2024-02-02 14:01:30 -0500

    Color-setting Chips

---
+++ src/common/utils/enumColorUtils.ts
@@ -0,0 +1,19 @@
+import { Genre, Role } from '../constants/enums'
+
+export function getColorForEnum(arg: Genre | Role): string {
+  switch (arg) {
+    case 'documentary':
+      return '#FECBA1'
+    case 'animation':
+      return '#A3CFBB'
+    // There isn't any styling for Series in Figma
+    case 'series':
+      return 'your_series_color'
+    case 'camera_operator':
+      return '#C29EFA'
+    case 'director_producer':
+      return '#9EC5FE'
+    default:
+      return ''
+  }
+}
--- src/components/atoms/chipsForEnum/ChipsForEnum.tsx
+++ src/components/atoms/chipsForEnum/ChipsForEnum.tsx
@@ -1,29 +1,31 @@
+// This file functions, and renders, perfectly fine
+// There is an error "squiggle" on the very last curly bracket
+// that says, "Declaration or statement expected.ts(1128)"???
 import Chip from '@mui/material/Chip'
-import { Genre, Role, roleTypeChecker } from '../../../common/constants/enums'
+import { Genre, Role, genreTypeChecker } from '../../../common/constants/enums'
 import {
   convertFromEnumToText,
   convertFromEnumToUrl,
 } from '../../../common/utils/utils'
+import { getColorForEnum } from '../../../common/utils/enumColorUtils'
 
-export function ChipsForEnum({ value }: { value: Genre | Role }) {
-  const buildChipProps = (arg: Genre | Role): any => {
-    const filterKey = roleTypeChecker(arg) ? 'role' : 'genre'
-    const chipProps: any = {
-      label: convertFromEnumToText(arg),
-      clickable: true,
-    }
+export function ChipsForEnum({ value }: { value: Genre | Role }): JSX.Element {
+  const isGenre = genreTypeChecker(value)
+  const filterKey = isGenre ? 'genre' : 'role'
+  const label = convertFromEnumToText(value)
+  const backgroundColor = getColorForEnum(value)
 
-    if (chipProps.clickable) {
-      chipProps.component = 'button'
-      chipProps.onClick = () => {
-        const url =
-          '/video-list-page?' + filterKey + '=' + convertFromEnumToUrl(arg)
-        window.location.href = url
-      }
-    }
-
-    return chipProps
+  const handleClick = () => {
+    const url = `/video-list-page?${filterKey}=${convertFromEnumToUrl(value)}`
+    window.location.href = url
   }
 
-  return <Chip {...buildChipProps(value)} />
-} 
+  return (
+    <Chip
+      label={label}
+      component="button"
+      onClick={handleClick}
+      style={{ backgroundColor: backgroundColor as any }}
+    />
+  )
+}
