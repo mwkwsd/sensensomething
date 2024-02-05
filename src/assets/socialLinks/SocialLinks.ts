0c74a07a8db1948bb657110feb6005517473466e
Mark <markwilkins86@gmail.com>
2024-02-05 15:38:20 -0500

    Working Bottom Icon Links

---
+++ src/assets/socialLinks/SocialLinks.ts
@@ -0,0 +1,12 @@
+// Allows Kurt to add additional Social Links'
+// as well as easily update URLs if they change
+
+interface SocialLinks {
+  vimeo: string;
+  instagram: string;
+}
+
+export const socialLinks: SocialLinks = {
+  vimeo: 'https://www.vimeo.com/',
+  instagram: 'https://www.instagram.com/',
+};
---
+++ src/components/atoms/footer/IconNavigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+interface IconNavigationProps {
+  icon: React.ReactNode;
+  href: string;
+  sx?: React.CSSProperties; // Add this line to include styling prop
+}
+
+export function IconNavigation({ icon, href, sx }: IconNavigationProps): JSX.Element {
+  return (
+    <BottomNavigationAction
+      icon={icon}
+      sx={{ color: 'white', fontSize: '30px', ...sx }} 
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+    />
+  );
+}
