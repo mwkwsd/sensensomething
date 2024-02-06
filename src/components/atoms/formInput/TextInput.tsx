6758a93b52f6c1e5bceff2f5ff955e262e41dc4e
WillyLS <WLScripps@gmail.com>
2024-02-05 18:55:46 -0700

    Add util type for required field

--- src/components/atoms/formInput/TextInput.tsx
+++ src/components/atoms/formInput/TextInput.tsx
@@ -2,16 +2,16 @@ import { TextField, TextFieldProps } from "@mui/material";
 import { Input } from "../../../common/constants/enums";
 import { kurtTheme } from "../../../theme";
 import { inputToFormProps } from "../../../common/constants/constants";
+import { RequiredFields } from "../../../common/utils/typescriptUtils";
 
 export type TextInputProps = {
   fieldName: Input
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-} & TextFieldProps
+} & RequiredFields<TextFieldProps, 'onChange'>
 
 export function TextInput({fieldName, ...rest}: TextInputProps) {
 
   return (
-    <TextField 
+    <TextField
       sx={{backgroundColor: kurtTheme.palette.background.default, marginY: 2, maxWidth: 400, height: rest.multiline ? 100 : undefined}}
       id={fieldName + "_text"}
       label={inputToFormProps[fieldName]["label"]}
