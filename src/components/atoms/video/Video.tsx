602fb83879b7bc9b1a1bf78138a3d165af8ec6c1
Mark Wilkins <102488171+mwilkins74@users.noreply.github.com>
2024-02-06 22:44:51 -0500

    utilizing ReactPlayer to render Videos (#61)

--- src/components/atoms/video/Video.tsx
+++ src/components/atoms/video/Video.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import ReactPlayer from 'react-player'
 import { CardMedia } from '@mui/material'
 
 type VideoProps = {
   url: string
 }
 
-// WIP
 export function Video({ url }: VideoProps) {
   return (
-    <CardMedia
-      component={'iframe'}
-      src={addQueryParamsToVideoUrl(url)}
-      sx={{ aspectRatio: '16/9', border: '0' }}
-    />
+    <CardMedia component={'div'} sx={{ aspectRatio: '16/9', border: '0' }}>
+      <ReactPlayer
+        url={addQueryParamsToVideoUrl(url)}
+        controls
+        width="100%"
+        height="100%"
+      />
+    </CardMedia>
   )
 }
+
 function addQueryParamsToVideoUrl(url: string): string {
+  // Add any query parameters if needed
   // color only seems to change the color of the played % on the video controls
   // Not any of the buttons. Might need to look at Kurt's setting for that.
   return url + '?color=0000ff'
