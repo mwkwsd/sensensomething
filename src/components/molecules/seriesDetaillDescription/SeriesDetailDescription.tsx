e047da35fdae0a86e0777c8b4dc01a1f0377634c
WillyLS <WLScripps@gmail.com>
2024-02-07 18:30:19 -0700

    Stupid linter

--- src/components/molecules/seriesDetaillDescription/SeriesDetailDescription.tsx
+++ src/components/molecules/seriesDetaillDescription/SeriesDetailDescription.tsx
@@ -25,7 +25,7 @@ export function SeriesDetailDescription({
       <PageTitle title={pageTitle} />
       {seriesInfo.imageUrl && (
         <img
-          alt={`image for ${seriesInfo.name}`}
+          alt={`${seriesInfo.name}`}
           src={seriesInfo.imageUrl}
           width="100vw"
           style={{ marginLeft: '-16px' }}
