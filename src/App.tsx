3d448b60a120d79b29d243019c82c4bd52688530
Ken Dunn <kmdunn5@Kens-MacBook-Pro.local>
2024-02-09 19:05:57 -0500

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/templates/Home'
-import Contact from './components/templates/Contact'
 import { About } from './components/templates/About'
 import { VideoListPage } from './components/templates/VideoListPage'
 import { ThemeProvider } from '@mui/material/styles'
@@ -15,8 +14,8 @@ const router = createBrowserRouter([
     element: <KurtApp />,
     children: [
       {
-        path: '/contact',
-        element: <Contact />,
+        path: '/about',
+        element: <About />,
       },
       {
         path: '/videos',
