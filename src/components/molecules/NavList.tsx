1d75b3b967bdea81adab74c6894b3c1f4537e2ed
Mark <markwilkins86@gmail.com>
2024-02-22 13:57:43 -0500

    cleaned up `NavList`

--- src/components/molecules/NavList.tsx
+++ src/components/molecules/NavList.tsx
@@ -1,84 +1,52 @@
-import { List } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
-import {
-  Genre,
-  Role,
-  genres,
-  navLinks,
-  roles,
-} from '../../common/constants/enums'
-import { enumMappings } from '../../common/constants/constants'
-import { NavLinks } from './navLinks/NavLinks'
-import { NavStyle } from '../../common/utils/newStyle'
+import { List } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { navLinks, genres, roles } from '../../common/constants/enums';
+import { enumMappings } from '../../common/constants/constants';
+import { NavLinks } from './navLinks/NavLinks';
+import { NavStyle } from '../../common/utils/newStyle';
 
 type ListButtonProps = {
-  label: string
-  filter?: string
-  path: string
-}
+  label: string;
+  filter?: string;
+  path: string;
+};
+
+const orderedLinks: (typeof genres[number] | typeof roles[number])[] = [
+  'animation',
+  'documentary',
+  'director_of_photography',
+  'director_producer',
+  'series',
+];
 
 export function NavList() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const generateButtons = (
-    items: readonly (Genre | Role)[]
-  ): ListButtonProps[] =>
-    items.map(item => ({
-      label: enumMappings[item].label,
-      filter: item,
-      path: enumMappings[item].url,
-    }));
+  const combinedLinks: ListButtonProps[] = navLinks.map((item) => ({
+    label: item.label,
+    path: item.route,
+  }));
 
-  const combinedLinks: ListButtonProps[] = [
-    ...navLinks.map(item => ({
-      label: item.label,
-      path: item.route,
-    })),
-    ...generateButtons(roles),
-    ...generateButtons(genres),
-  ];
-
-  const secondNavLinkOrder = [
-    'Animation',
-    'Documentary',
-    'Director of Photography',
-    'Director & Producer',
-    'Series',
-  ];
-
-  const secondNavLinks = combinedLinks
-    .filter(item => secondNavLinkOrder.includes(item.label))
-    .sort((a, b) => secondNavLinkOrder.indexOf(a.label) - secondNavLinkOrder.indexOf(b.label));
+  const secondNavLinks = orderedLinks.map((linkEnum) => (
+    <NavLinks
+      key={`${linkEnum}`}
+      route={enumMappings[linkEnum].url}
+      label={enumMappings[linkEnum].label}
+      onClick={() => navigate(enumMappings[linkEnum].url)}
+    />
+  ));
 
   return (
     <div>
-      <List
-        sx={{ width: '100%', paddingBottom: '20px', maxWidth: 360, ...NavStyle }}
-        component="nav"
-      >
-        {navLinks.map((item, index) => (
-          <NavLinks
-            key={index}
-            route={item.route}
-            label={item.label}
-            onClick={() => navigate(item.route)}
-          />
+      <List sx={{ width: '100%', paddingBottom: '20px', maxWidth: 360, ...NavStyle }} component="nav">
+        {combinedLinks.map((item, index) => (
+          <NavLinks key={index} route={item.path} label={item.label} onClick={() => navigate(item.path)} />
         ))}
       </List>
 
-      <List
-        sx={{ width: '100%', height: '100%', maxWidth: 360, ...NavStyle }}
-        component="nav"
-      >
-        {secondNavLinks.map((item, index) => (
-          <NavLinks
-            key={index}
-            route={item.path}
-            label={item.label}
-            onClick={() => navigate(item.path)}
-          />
-        ))}
+      <List sx={{ width: '100%', height: '100%', maxWidth: 360, ...NavStyle }} component="nav">
+        {secondNavLinks}
       </List>
     </div>
-  )
+  );
 }
