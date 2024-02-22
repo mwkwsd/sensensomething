68d98dc7fb015a4853c704ff6670437adbc4afb9
Mark <markwilkins86@gmail.com>
2024-02-22 15:39:11 -0500

    forgot to remove route and keep onClick only

--- src/components/molecules/NavList.tsx
+++ src/components/molecules/NavList.tsx
@@ -1,52 +1,66 @@
-import { List } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { navLinks, genres, roles } from '../../common/constants/enums';
-import { enumMappings } from '../../common/constants/constants';
-import { NavLinks } from './navLinks/NavLinks';
-import { NavStyle } from '../../common/utils/newStyle';
+import { List } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { navLinks, genres, roles } from '../../common/constants/enums'
+import { enumMappings } from '../../common/constants/constants'
+import { NavLinks } from './navLinks/NavLinks'
+import { NavStyle } from '../../common/utils/newStyle'
 
 type ListButtonProps = {
-  label: string;
-  filter?: string;
-  path: string;
-};
+  label: string
+  filter?: string
+  path: string
+}
 
-const orderedLinks: (typeof genres[number] | typeof roles[number])[] = [
+const orderedLinks: ((typeof genres)[number] | (typeof roles)[number])[] = [
   'animation',
   'documentary',
   'director_of_photography',
   'director_producer',
   'series',
-];
+]
 
 export function NavList() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const combinedLinks: ListButtonProps[] = navLinks.map((item) => ({
+  const combinedLinks: ListButtonProps[] = navLinks.map(item => ({
     label: item.label,
     path: item.route,
-  }));
+  }))
 
-  const secondNavLinks = orderedLinks.map((linkEnum) => (
+  const secondNavLinks = orderedLinks.map(linkEnum => (
     <NavLinks
       key={`${linkEnum}`}
-      route={enumMappings[linkEnum].url}
       label={enumMappings[linkEnum].label}
       onClick={() => navigate(enumMappings[linkEnum].url)}
     />
-  ));
+  ))
 
   return (
     <div>
-      <List sx={{ width: '100%', paddingBottom: '20px', maxWidth: 360, ...NavStyle }} component="nav">
+      <List
+        sx={{
+          width: '100%',
+          paddingBottom: '20px',
+          maxWidth: 360,
+          ...NavStyle,
+        }}
+        component="nav"
+      >
         {combinedLinks.map((item, index) => (
-          <NavLinks key={index} route={item.path} label={item.label} onClick={() => navigate(item.path)} />
+          <NavLinks
+            key={index}
+            label={item.label}
+            onClick={() => navigate(item.path)}
+          />
         ))}
       </List>
 
-      <List sx={{ width: '100%', height: '100%', maxWidth: 360, ...NavStyle }} component="nav">
+      <List
+        sx={{ width: '100%', height: '100%', maxWidth: 360, ...NavStyle }}
+        component="nav"
+      >
         {secondNavLinks}
       </List>
     </div>
-  );
+  )
 }
