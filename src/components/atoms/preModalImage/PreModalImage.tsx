fee8110857b7471f834e25ebe95d5984d0f38c6a
WillyLS <WLScripps@gmail.com>
2024-03-03 12:46:18 -0700

    Use images

--- src/components/atoms/preModalImage/PreModalImage.tsx
+++ src/components/atoms/preModalImage/PreModalImage.tsx
@@ -1,21 +1,43 @@
-import { Button } from '@mui/material'
-import { useContext } from 'react'
+import { Box, Typography } from '@mui/material'
+import { ReactNode, useContext, useMemo } from 'react'
 import { IVideoInfo } from '../../../common/interfaces/IVideoInfo'
+import {
+  getVideoProviderAndId,
+  getVideoThumnailUrl,
+} from '../../../common/utils/thumbnailUrlUtils'
 import { ModalContext } from '../../organisms/modal/ModalProvider'
 
 type PreModalImageProps = {
   videoInfo: IVideoInfo
 }
 
-export function PreModalImage({
-  videoInfo: { url, title },
-}: PreModalImageProps) {
+function buildImageComponentWithSource(
+  source: string,
+  title: string
+): ReactNode {
+  return <img src={source} alt={title} width={'100%'} />
+}
+
+export function PreModalImage({ videoInfo }: PreModalImageProps) {
+  const { thumbnailPath, title, url } = videoInfo
   const { showModal } = useContext(ModalContext)
 
   // TODO Fix the below in #93
-  return (
-    <Button
-      onClick={() => showModal(url)}
-    >{`Click me for ${title} video!`}</Button>
-  )
+
+  const imageComponent = useMemo(() => {
+    if (thumbnailPath) {
+      return buildImageComponentWithSource(thumbnailPath, title)
+    }
+    const [provider, id] = getVideoProviderAndId(videoInfo.url)
+    if (provider === 'unknown') {
+      return <Typography>{title}</Typography>
+    }
+
+    return buildImageComponentWithSource(
+      getVideoThumnailUrl(provider, id),
+      title
+    )
+  }, [videoInfo])
+
+  return <Box onClick={() => showModal(url)}>{imageComponent}</Box>
 }
