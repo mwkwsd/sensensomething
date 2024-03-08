50ae4d49e7a5a186dcc121416a7c8d707e636cf5
Mark <markwilkins86@gmail.com>
2024-03-08 15:38:50 -0500

    show more button and limit videos scrolls

--- src/components/organisms/videoList/VideoList.tsx
+++ src/components/organisms/videoList/VideoList.tsx
@@ -2,6 +2,8 @@ import { Grid } from '@mui/material'
 import { IVideoInfo } from '../../../common/interfaces/IVideoInfo'
 import { PageTitle } from '../../atoms/pageTitle/PageTitle'
 import { VideoCard } from '../../molecules/videoCard/VideoCard'
+import { ShowMoreButton } from '../../atoms/buttons/ShowMoreButton'
+import { useState } from 'react'
 
 type VideoListProps = {
   videos: IVideoInfo[]
@@ -9,20 +11,41 @@ type VideoListProps = {
 }
 
 export function VideoList({ videos, pageType }: VideoListProps) {
-  const videoComponents = videos.map(video => (
+  const videosPerPage = 3
+  const [visibleVideos, setVisibleVideos] = useState(
+    videos.slice(0, videosPerPage)
+  )
+
+  const handleShowMore = () => {
+    const currentVisibleCount = visibleVideos.length;
+    const nextVisibleVideos = videos.slice(currentVisibleCount, currentVisibleCount + videosPerPage);
+    setVisibleVideos((prevVisibleVideos: IVideoInfo[]) => [...prevVisibleVideos, ...nextVisibleVideos]);
+  };
+
+  const allVideosDisplayed = visibleVideos.length === videos.length;
+
+  const videoComponents = visibleVideos.map((video: IVideoInfo) => (
     <Grid item xs={12} md={6} key={`grid-item-video-list-${video.title}`}>
       <VideoCard video={video} pageType={pageType} />
     </Grid>
   ))
+  console.log(videoComponents)
   return (
     <>
       <PageTitle
         title="RECENT WORK"
-        sx={{ padding: '32px 16px 16px', fontSize: '1.75rem', fontWeight: '400' }}
+        sx={{
+          padding: '32px 16px 16px',
+          fontSize: '1.75rem',
+          fontWeight: '400',
+        }}
       />
       <Grid container spacing={2}>
         {videoComponents}
       </Grid>
+      {!allVideosDisplayed && (
+        <ShowMoreButton onClick={handleShowMore} />
+      )}
     </>
   )
 }
