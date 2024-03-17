9ff8d0144772f50c8ed3da6f4871d940b69ca998
Mark Wilkins <102488171+mwilkins74@users.noreply.github.com>
2024-03-17 12:30:52 -0400

    show more button and limit videos scrolls (#131)
    * show more button and limit videos scrolls
    
    * `HireMeButton` added
    
    * requested changes addressed

--- src/components/organisms/videoList/VideoList.tsx
+++ src/components/organisms/videoList/VideoList.tsx
@@ -2,18 +2,38 @@ import { Grid } from '@mui/material'
 import { IVideoInfo } from '../../../common/interfaces/IVideoInfo'
 import { PageTitle } from '../../atoms/pageTitle/PageTitle'
 import { VideoCard } from '../../molecules/videoCard/VideoCard'
+import { ShowMoreButton } from '../../atoms/buttons/ShowMoreButton'
+import { HireMeButton } from '../../atoms/buttons/HireMeButton'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 type VideoListProps = {
   videos: IVideoInfo[]
   pageType: 'genre' | 'role' | 'recent'
 }
 
+const videosPerPage = 3
+
 export function VideoList({ videos, pageType }: VideoListProps) {
-  const videoComponents = videos.map(video => (
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
+
+  const [visibleVideos, setVisibleVideos] = useState(
+    videos.slice(0, videosPerPage)
+  )
+
+  const handleShowMore = () => {
+    setVisibleVideos(videos.slice(0, visibleVideos.length + videosPerPage))
+  }
+
+  const allVideosDisplayed = visibleVideos.length === videos.length
+
+  const videoComponents = visibleVideos.map((video: IVideoInfo) => (
     <Grid item xs={12} md={6} key={`grid-item-video-list-${video.title}`}>
       <VideoCard video={video} pageType={pageType} />
     </Grid>
   ))
+  console.log(videoComponents)
   return (
     <>
       <PageTitle
@@ -22,6 +42,8 @@ export function VideoList({ videos, pageType }: VideoListProps) {
       <Grid container spacing={2}>
         {videoComponents}
       </Grid>
+      {!isHomePage && <HireMeButton />}
+      {!allVideosDisplayed && <ShowMoreButton onClick={handleShowMore} />}
     </>
   )
 }
