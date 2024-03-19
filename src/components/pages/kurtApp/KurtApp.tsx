0a14d2fbc2745a0123f2a2b2c2421f3e6998dea1
Mark <markwilkins86@gmail.com>
2024-03-19 17:50:40 -0400

    fixing deploy error

--- src/components/pages/kurtApp/KurtApp.tsx
+++ src/components/pages/kurtApp/KurtApp.tsx
@@ -1,6 +1,5 @@
 import { useMemo } from 'react'
 import { Outlet } from 'react-router-dom'
-import { ScrollToHashElement } from '../../atoms/utilityComponents/ScrollToHashElement'
 import { Footer } from '../../organisms/Footer/Footer'
 import { VideoModal } from '../../organisms/modal/VideoModal'
 import { NavBar } from '../../templates/NavBar'
@@ -10,7 +9,6 @@ export function KurtApp() {
   const footer = useMemo(() => <Footer />, [])
   return (
     <>
-      <ScrollToHashElement />
       {navBar}
       <VideoModal />
       <Outlet />
