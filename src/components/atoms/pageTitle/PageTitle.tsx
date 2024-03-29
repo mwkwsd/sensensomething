9754ec82407c4a3007c8e587d1e083108e6f4dd6
Mark <markwilkins86@gmail.com>
2024-03-29 15:15:45 -0400

    removed Spaceholder from VideoList

--- src/components/atoms/pageTitle/PageTitle.tsx
+++ src/components/atoms/pageTitle/PageTitle.tsx
@@ -13,24 +13,25 @@ export function PageTitle({
   titleUnderlineColor,
   subheaderText,
   sx,
-  fullWidth
+  fullWidth,
 }: PageTitleProps) {
-  const titleUnderline: SxProps = titleUnderlineColor
-    ? {
-        paddingBottom: '16px',
-        width: fullWidth ? '100%' : '40px',
-        ':after': {
-          border: `1px solid ${titleUnderlineColor}`,
-          display: 'block',
-          content: '""',
-          width: fullWidth ? '100%' : '40px',
-        },
-      }
-    : { paddingBottom: '16px' }
-
   return (
     <Container sx={{ ...sx }} disableGutters={true}>
-      <Typography variant="h1" sx={titleUnderline}>
+      <Typography
+        variant="h1"
+        sx={{
+          paddingBottom: subheaderText ? '16px' : '0', 
+          width: fullWidth ? '100%' : '40px',
+          ':after': {
+            border: titleUnderlineColor
+              ? `1px solid ${titleUnderlineColor}`
+              : 'none',
+            display: 'block',
+            content: '""',
+            width: fullWidth ? '100%' : '40px',
+          },
+        }}
+      >
         {title?.toLocaleUpperCase()}
       </Typography>
       {subheaderText && (
