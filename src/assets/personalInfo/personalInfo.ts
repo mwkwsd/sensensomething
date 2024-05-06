7720b567d3fa6dfd982ba583fba74000de436836
Mark <markwilkins86@gmail.com>
2024-05-06 10:28:20 -0400

    formatting

--- src/assets/personalInfo/personalInfo.ts
+++ src/assets/personalInfo/personalInfo.ts
@@ -1,11 +1,11 @@
 interface IPersonalPhoto {
-  path: string;
-  alt: string;
+  path: string
+  alt: string
 }
 
 interface IPersonalInfoItem {
-  description: string;
-  photo: IPersonalPhoto;
+  description: string
+  photo: IPersonalPhoto
 }
 
 export const Kurt: IPersonalInfoItem[] = [
@@ -19,25 +19,25 @@ export const Kurt: IPersonalInfoItem[] = [
   
   Kurt has a Master of Fine Arts in Documentary Studies and Production from Hofstra University and a Bachelor of Fine Arts in Film from the University of Wisconsin-Milwaukee.`,
     photo: {
-      path: "/images/kurt_profile.png",
-      alt: "Kurt Sensenbrenner flying an airplane"
-    }
+      path: '/images/kurt_profile.png',
+      alt: 'Kurt Sensenbrenner flying an airplane',
+    },
   },
   {
     description: `Morbi posuere leo eros mauris ut magnis fames. Facilisis diam at iaculis metus. Mi commodo ligula feugiat nulla mauris pharetra ipsum nulla mauris. Tristique fames in tempus ornare elit ipsum suspendisse ultricies porttitor. 
 
     Dolor lacus mauris varius est orci condimentum. Mi interdum purus sit egestas elementum enim. Egestas vitae condimentum lorem sollicitudin aenean feugiat. `,
     photo: {
-      path: "/images/kurt_and_friend.png",
-      alt: "Kurt Sensenbrenner and a friend"
-    }
+      path: '/images/kurt_and_friend.png',
+      alt: 'Kurt Sensenbrenner and a friend',
+    },
   },
   {
     description: `Nec morbi nisi tortor elit rutrum fringilla. Felis aliquam magna scelerisque tempus donec mollis. Viverra mauris suscipit interdum tristique porttitor vestibulum. Eu neque integer consequat ipsum elementum posuere vel non in. Pulvinar tincidunt laoreet ac est id orci sed sociis. Fringilla aliquam mi mattis dictum. Dolor in mauris enim risus.`,
     photo: {
-      path: "/images/kurt_and_people.png",
-      alt: "Kurt Sensenbrenner and people smiling"
-    }
+      path: '/images/kurt_and_people.png',
+      alt: 'Kurt Sensenbrenner and people smiling',
+    },
   },
   // Add more items as needed
-];
+]
