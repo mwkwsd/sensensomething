f2916752982963ef0d45db8a4f01132e32d5a92f
WillyLS <WLScripps@gmail.com>
2025-03-16 12:55:05 -0600

    Added 'key' prop to VideoList

--- src/assets/pages/documentary.ts
+++ src/assets/pages/documentary.ts
@@ -3,8 +3,8 @@ import { IVideoListPageInfo } from '../../common/interfaces/IVideoListPageInfo'
 export const documentaryPageInfo: IVideoListPageInfo = {
   pageTitle: 'Documentary',
   videoTitles: [
-    'From Mass to the Mountain',
     'Meet The Pill Bug',
+    'From Mass to the Mountain',
     'Afro Punk Ballet',
     'How To: Go Wicky-Wacky with Bridge',
   ],
--- src/components/templates/VideoListPage.tsx
+++ src/components/templates/VideoListPage.tsx
@@ -1,23 +1,23 @@
-import { useMemo } from 'react'
-import { IVideoInfo } from '../../common/interfaces/IVideoInfo'
-import { VideoList } from '../organisms/videoList/VideoList'
-import { PageTitle } from '../atoms/pageTitle/PageTitle'
-import { useLoaderData } from 'react-router-dom'
-import { VideoPageArgs, getVideoInfoForTitle } from '../../common/utils/utils'
 import { useTheme } from '@mui/system'
+import { useMemo } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import { IVideoInfo } from '../../common/interfaces/IVideoInfo'
+import { VideoPageArgs, getVideoInfoForTitle } from '../../common/utils/utils'
+import { PageTitle } from '../atoms/pageTitle/PageTitle'
+import { VideoList } from '../organisms/videoList/VideoList'
 
 /**
  * @returns Page for showing video lists for Roles (Director & Producer, Director of Photography) and Documentary
  */
 export function VideoListPage() {
-  const [videoPageInfo, pageEnum, pageType] = useLoaderData() as VideoPageArgs
+  const [videoPageInfo, pageEnum, pageType] = useLoaderData<VideoPageArgs>()
   const theme = useTheme()
 
   const videosForPage = useMemo(
     () =>
       videoPageInfo.videoTitles
         .map(videoTitle => getVideoInfoForTitle(videoTitle))
-        .filter((video): video is NonNullable<IVideoInfo> => !!video),
+        .filter((video): video is IVideoInfo => !!video),
     [videoPageInfo.videoTitles]
   )
 
@@ -29,9 +29,13 @@ export function VideoListPage() {
         title={videoPageInfo.pageTitle}
         titleUnderlineColor={titleUnderlineColor}
         subheaderText={videoPageInfo.subheaderText}
-        sx={{ padding: '16px 16px 24px' }}
+        sx={{ padding: '1rem 1rem 1.5rem' }}
       />
-      <VideoList videos={videosForPage} pageType={pageType} />
+      <VideoList
+        key={`video-list-${pageType}`}
+        videos={videosForPage}
+        pageType={pageType}
+      />
     </>
   )
 }
